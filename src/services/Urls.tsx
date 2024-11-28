ae3f0429c148718db9f4c20a766dca2486e52191
pawel.miera <pawel.miera@rad4m.com>
2024-11-28 19:58:31 +0100

    feat: mobile improvements

--- src/services/Urls.tsx
+++ src/services/Urls.tsx
@@ -1,11 +1,11 @@
 const production = 'https://mhosting.ipv64.net/groopieApi';
-const development = 'http://localhost:8000/groopieApi';
+const development = 'http://192.168.0.107:8000/groopieApi';
 
 const production_front = 'https://mhosting.ipv64.net/groopie';
-const development_front = 'http://localhost:5173/groopie';
+const development_front = 'http://192.168.0.107:5173/groopie';
 
 const production_ws = 'wss://mhosting.ipv64.net/groopieWs/';
-const development_ws = 'ws://localhost:8000/groopieWs/';
+const development_ws = 'ws://192.168.0.107:8000/groopieWs/';
 
 export const ApiUrl = process.env.NODE_ENV === 'development' ? development : production;
 export const FrontendUrl = process.env.NODE_ENV === 'development' ? development_front : production_front;
