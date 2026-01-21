6d69328e702246af48ce6b67eb3a90bad13268a9
pawel.miera <pawel.miera@rad4m.com>
2026-01-21 20:24:33 +0100

    update urls and packages

--- src/services/Urls.tsx
+++ src/services/Urls.tsx
@@ -1,10 +1,10 @@
-const production = 'https://mhosting.ipv64.net/groopieApi';
+const production = 'https://groopie.ipv64.net/groopieApi';
 const development = 'http://192.168.0.107:8000/groopieApi';
 
-const production_front = 'https://mhosting.ipv64.net/groopie';
-const development_front = 'http://192.168.0.107:5173/groopie';
+const production_front = 'https://groopie.ipv64.net/';
+const development_front = 'http://192.168.0.107:5173/';
 
-const production_ws = 'wss://mhosting.ipv64.net/groopieWs/';
+const production_ws = 'wss://groopie.ipv64.net/groopieWs/';
 const development_ws = 'ws://192.168.0.107:8000/groopieWs/';
 
 export const ApiUrl = process.env.NODE_ENV === 'development' ? development : production;
